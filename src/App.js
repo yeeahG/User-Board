98a6ec201684446e118592bcc5f050d5f77c56a4
yeeahG <yeji.sep.k@gmail.com>
2022-04-22 00:02:43 +0900

    [Files]

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movie Review</h1>
+      <div className='movie-container'>
+        <h2>제목</h2>
+        <div>
+          내용
+        </div>
+      </div>
+      <div className='form-wrapper'>
+        <input className="title-input" type='text' placeholder='제목' />
+        <textarea className="text-area" placeholder='내용'></textarea>
+      </div>
+      <button className="submit-button">입력</button>
     </div>
   );
 }
