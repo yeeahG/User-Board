364aff991123ee4ddb6b54d39eb221fcf8611370
yeeahG <yeji.sep.k@gmail.com>
2022-04-22 01:12:54 +0900

    [Board]

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import './App.css';
+import React, { Component } from 'react';
+import Menu from './components/Menu'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Movie Review</h1>
-      <div className='movie-container'>
-        <h2>제목</h2>
-        <div>
-          내용
-        </div>
-      </div>
-      <div className='form-wrapper'>
-        <input className="title-input" type='text' placeholder='제목' />
-        <textarea className="text-area" placeholder='내용'></textarea>
-      </div>
-      <button className="submit-button">입력</button>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Menu />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import './Menu.css'
+
+const Menu = () => {
+    // const [movieContent, setMovieContent] = useState({
+    //     title: '',
+    //     content: ''
+    // })
+    
+      const getValue = e => {
+        const { name, value } = e.target;
+        console.log(name, value);
+    };
+    
+
+  return (
+    <div className="App">
+        <h1>Movie Review</h1>
+        <div className='movie-container'>
+
+        </div>
+        <div className="form-wrapper">
+            <input 
+                className="title-input"
+                type='text'
+                placeholder='제목'
+                onChange={getValue}
+                name='title'
+            />
+        </div>
+
+        <h2>Using CKEditor 5 build in React</h2>
+        <CKEditor
+            editor={ ClassicEditor }
+            data="<p>Hello from CKEditor 5!</p>"
+            onReady={ editor => {
+            // You can store the "editor" and use when it is needed.
+            console.log( 'Editor is ready to use!', editor );
+            } }
+            onChange={ ( event, editor ) => {
+                const data = editor.getData();
+                console.log( { event, editor, data } );
+            } }
+            onBlur={ ( event, editor ) => {
+                console.log( 'Blur.', editor );
+                } }
+            onFocus={ ( event, editor ) => {
+                console.log( 'Focus.', editor );
+            } }
+        />
+
+        <button className="submit-button" >
+            입력
+        </button>
+    </div>
+  )
+}
+
+export default Menu;
